90039b6ff060caf6f1c85064793439c5165be326
RafsanProve <rafsanprove123@gmail.com>
2025-02-15 18:16:05 +0600

    Update user_model.py

--- server/models/user_model.py
+++ server/models/user_model.py
@@ -1,8 +1,9 @@
 class User:
-    def __init__(self, user_id, full_name, email, password_hash, account_balance, country, status):
+    def __init__(self, user_id, full_name, email, phone, password_hash, account_balance, country, status):
         self.user_id = user_id
         self.full_name = full_name
         self.email = email
+        self.phone = phone
         self.password_hash = password_hash
         self.account_balance = account_balance
         self.country = country
